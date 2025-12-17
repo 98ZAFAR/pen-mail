0ddb84cf4958ba057d841698ac83408f412769b9
Md Zafar <mz5659875@gmail.com>
2025-12-17 14:56:03 +0530

    Frontend Initialized

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main className='flex justify-center items-center min-h-screen w-full bg-gray-100'>
+      <h2 className='text-5xl text-gray-800 font-semibold font-nunito'>Welcome to Pen Mail</h2>
+    </main>
+  )
+}
